cf5f74a8e0af2555d2e52287d8bc098f12cbd292
dongyeonkim <ehd9567@naver.com>
2023-03-06 16:36:52 +0900

    Task용 DAO생성

--- src/main/java/com/example/my_schedules/dao/TaskDAO.java
+++ src/main/java/com/example/my_schedules/dao/TaskDAO.java
@@ -7,4 +7,11 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface TaskDAO {
     public List<TaskDTO> getAllTask();
+    public TaskDTO gatTaskById(int id);
+
+    public int setTask(TaskDTO taskDTO);
+
+    public int putTask(TaskDTO taskDTO);
+
+    public int deleteTask(int task_id);
 }
