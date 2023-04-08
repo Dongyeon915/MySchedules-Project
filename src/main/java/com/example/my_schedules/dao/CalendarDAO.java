997eaf9a4e55e96140a37607f79548df6bd05627
dongyeonkim <ehd9567@naver.com>
2023-04-08 16:19:53 +0900

    캘린더 연습

--- src/main/java/com/example/my_schedules/dao/CalendarDAO.java
+++ src/main/java/com/example/my_schedules/dao/CalendarDAO.java
@@ -9,4 +9,5 @@ import org.apache.ibatis.annotations.Mapper;
 public interface CalendarDAO {
     //    달력을 위한 해당유저의 정보를 가져오는 메소드
     public TodoResultDTO getCalendarUserId(TodoResultDTO todoResultDTO);
+    public List<TodoResultDTO> allCalendarUserId(TodoResultDTO todoResultDTO);
 }
