9c7f330a7d84fd0be630d09a47351201c1f25977
dongyeonkim <ehd9567@naver.com>
2023-04-08 16:21:37 +0900

    게시판기능DAO생성

---
+++ src/main/java/com/example/my_schedules/dao/TilDAO.java
@@ -0,0 +1,20 @@
+package com.example.my_schedules.dao;
+
+import com.example.my_schedules.dto.TilDTO;
+import com.example.my_schedules.dto.TuiDTO;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TilDAO {
+
+    public List<TilDTO> getUsetByTilContent();
+
+    public TilDTO getTilContentById(int id);
+
+    public int setContent(TilDTO TilDTO);
+    
+    public int updateContent(TilDTO TilDTO);
+    
+    public int deleteContent(int id);
+}
