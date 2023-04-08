5b6969026e5eea1a85240d2a01aa9532a1fdbb5c
dongyeonkim <ehd9567@naver.com>
2023-04-08 16:21:06 +0900

    게시판기능 작업

---
+++ src/main/java/com/example/my_schedules/controller/todo_controller/TilController.java
@@ -0,0 +1,53 @@
+package com.example.my_schedules.controller.todo_controller;
+
+import com.example.my_schedules.dao.TilDAO;
+import com.example.my_schedules.dto.TilDTO;
+import com.example.my_schedules.dto.TuiDTO;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/til")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TilController {
+
+    @Autowired
+    TilDAO tilDAO;
+
+    @GetMapping
+    public List<TilDTO> getUsetByTilContent(){
+        return tilDAO.getUsetByTilContent();
+    }
+
+    @GetMapping("/{id}")
+    public TilDTO getTilContentById(@PathVariable int id){
+        return tilDAO.getTilContentById(id);
+    }
+
+    @PostMapping
+    public int setContent(@RequestBody TilDTO tilDTO){
+        tilDAO.setContent(tilDTO);
+        return tilDTO.getTuiId();
+    }
+
+    @PutMapping
+    public int updateContent(@RequestBody TilDTO tilDTO){
+        tilDAO.updateContent(tilDTO);
+        return tilDTO.getTuiId();
+    }
+
+    @DeleteMapping("/{id}")
+    public int deleteContent(@PathVariable int id){
+        return tilDAO.deleteContent(id);
+    }
+
+}
