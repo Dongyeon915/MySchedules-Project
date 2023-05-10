cc142ecac77b536b8f704e5b0269f16f3588e47b
dongyeonkim <ehd9567@naver.com>
2023-05-10 21:02:48 +0900

    context정보 controller

---
+++ src/main/java/com/example/my_schedules/controller/todo_controller/SecurityController.java
@@ -0,0 +1,53 @@
+package com.example.my_schedules.controller.todo_controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class SecurityController {
+
+    @GetMapping("/login")
+    public ResponseEntity<Object> loginFailHandler() {
+        return new ResponseEntity<Object>("FUCK YOU Bad User!", HttpStatus.UNAUTHORIZED);
+    }
+    @GetMapping("/context")
+    public Object getContextInfo() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return securityContext;
+    }
+
+
+    @GetMapping("/authentication")
+    public Object getAuthenticationInfo() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        return authentication;
+    }
+
+    @GetMapping("/authentication/principal")
+    public Object getPrincipalInfo() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+
+    @GetMapping("/authentication/credential")
+    public Object getcredentialInfo() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        return authentication.getCredentials();
+    }
+
+    @GetMapping("/authentication/authorities")
+    public Object getAuthoritiesInfo() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+}
