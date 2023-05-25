3a5a222c88addb340ecbd1e57584707015cb302e
dongyeonkim <ehd9567@naver.com>
2023-05-25 14:36:29 +0900

    네이버 회원정보용 바이트배열 변환 과정 추가

--- src/main/java/com/example/my_schedules/filterDto/MyOAuth2UserService.java
+++ src/main/java/com/example/my_schedules/filterDto/MyOAuth2UserService.java
@@ -18,12 +18,19 @@ import org.springframework.stereotype.Service;
 // db에 저장된 사용자를 가져올때 사용
 // DaoAuthenticationProvider는 반드시 1개의 UserDetailsService를 발견할 수 있어야 한다. 만약 없으면
 // InmemoryUserDetailsManager에 [username=user, password=(서버가 생성한 패스워드)]인 사용자가 등록되어 제공됩니다.
+// 사용자 정보를 요청할 수 있는 access token 을 얻고나서 실행
 @Service
 public class MyOAuth2UserService extends DefaultOAuth2UserService {
 
     @Autowired
     Oauth2Dao oauth2Dao;
 
+// access token을 이용해 서드파티 서버로부터 사용자 정보를 받아온다.
+// 해당 사용자가 이미 회원가입 되어있는 사용자인지 확인한다.
+// 만약 회원가입이 되어있지 않다면, 회원가입 처리한다.
+// 만약 회원가입이 되어있다면, 프로필사진URL 등의 정보를 업데이트한다.
+// UserPrincipal 을 return 한다.
+// 세션 방식에서는 여기서 return한 객체가 시큐리티 세션에 저장된다.
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         System.out.println("유저 서비스 구역");
@@ -32,21 +39,34 @@ public class MyOAuth2UserService extends DefaultOAuth2UserService {
         String clientName = userRequest.getClientRegistration().getClientName().toUpperCase();
         Map<String, Object> attributes = AttributeUtil.getOAuthAttribute(clientName,
             oAuth2User.getAttributes());
-
         String email = (String) attributes.get("email");
         MyUserDTO user = oauth2Dao.getUserByEmail(email);
             // 회원가입 작성 !
             if (oauth2Dao.getUserByEmail(email) == null){
-                    MyUserDTO myUser = MyUserDTO.builder().userID((String) attributes.get("id"))
+                user = MyUserDTO.builder().userID((String) attributes.get("id"))
                         .name((String) attributes.get("name")).profile(
                             (String) attributes.get("profile_image")).email(
                             (String) attributes.get("email")).build();
-                    oauth2Dao.saveUser(myUser);
+
+//              네이버는 카카오와 다르게 넘버형이기에 변환 해서 넣어줫다
+                String idValue = user.getUserID();
+                byte[] bytes = idValue.getBytes();
+                int num = 0;
+                for (int i = 0; i < bytes.length; i++) {
+                   num += bytes[i];
+                }
+                user.setUserID(String.valueOf(num));
+
+                    oauth2Dao.saveUser(user);
             }else if (oauth2Dao.getUserByEmail(email) != null){
-                MyUserDTO myUserDTO = MyUserDTO.builder().userID((String) attributes.get("id")).name(
-                    String.valueOf(attributes.get("name"))).profile(
-                    (String) attributes.get("profile_image")).build();
-                oauth2Dao.updateUser(myUserDTO);
+                user = MyUserDTO.builder().userID((String)
+                        attributes.get("id"))
+                    .name(String.valueOf(attributes.get("name")))
+                    .profile((String) attributes.get("profile_image"))
+                    .build();
+                String id = user.getUserID();
+
+                oauth2Dao.updateUser(user);
             }
         user.setAttributes(attributes);
         user.setAuthorities(oAuth2User.getAuthorities());
